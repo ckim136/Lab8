07a02633f0ee7e310e43551b603b0262f0e24db7
CHAEEUN KIM <ckim136@my.bcit.ca>
2019-11-20 16:44:04 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Lab8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab8.dir/main.cpp.o"
+  "Lab8.pdb"
+  "Lab8.exe"
+  "libLab8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
